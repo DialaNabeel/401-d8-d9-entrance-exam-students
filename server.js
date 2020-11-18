c53e2d5cc3aef277166dad34b72b2cd66221c71d
MsDiala <diala-is-diala@hotmail.com>
2020-11-18 12:12:27 +0200

    gg

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express');
 const pg = require('pg');
 const superagent = require('superagent');
 const methodOverride = require('method-override');
+const { render } = require('ejs');
 
 // -------------------------
 // Environment variables
@@ -44,16 +45,91 @@ app.set('view engine', 'ejs');
 // ------- Routes -------
 // ----------------------
 
-
+app.get('/home',showHouse)
+app.get('/house_name',houseName)
+app.post('/house_name',addHouse)
+app.get('/house_name',renderFavorite)
+app.get('/character/character_id/:id',editChar)
+app.put('/character/character_id/:id',updateChar)
+app.delete('/character/character_id/:id',deleteChar)
 // --------------------------------
 // ---- Pages Routes functions ----
 // --------------------------------
+function showHouse(req,res) {
+    res.render('home')
+
+}
+
+///get from api
+function houseName(req,res) {
+    let arr=[];
+    let url = 'http://hp-api.herokuapp.com/api/characters/house/gryffindor';
+    superagent.get(url).then(data => {
+        data.body.forEach(element => {
+            arr.push(new hary(element));
+        });
+        res.render('characters',{result:arr});
+    }).catch(error => console.log(`Could not connect to database\n${error}`));
+
+}
 
 
+//constructor function for
+function hary(data){
+    this.name=data.name;
+    this.house=data.house;
+    this.patronus=data.patronus;
+}
 
 // -----------------------------------
 // --- CRUD Pages Routes functions ---
 // -----------------------------------
+
+//add to database
+function addHouse(req,res){
+    let query = 'INSERT INTO hary(name,house) VALUES ($1,$2);'
+   let values = [req.body.name,req.body.house]
+   client.query(query,values).then(()=>{
+       res.redirect('/house_name')
+   }).catch(error => console.log(`Could not connect to database\n${error}`)); 
+}
+
+//bring from database 
+function renderFavorite(req,res){
+    let query = 'SELECT * FROM hary;';
+    client.query(query).then(data=>{
+        res.render('characters',{result:data.rows});
+    }).catch(error => console.log(`Could not connect to database\n${error}`)); 
+
+}
+
+//edit char 
+function editChar(req,res){
+    let query = 'SELECT * FROM hary WHERE id=$1;'
+    let values =[req.params.id];
+    client.query(query).then(data=>{
+        res.render('characters',{result:data.rows[0]});
+    }).catch(error => console.log(`Could not connect to database\n${error}`)); 
+
+}
+
+//updateChar
+function updateChar(req,res){
+let query = 'UPDATE hary SET name=$1 , patronus=$2 WHERE id=$3;'
+let values= [req.body.name,req.body.patronus,req.params.id];
+client.query(query,values).then(()=>{
+    res.redirect('/character/character_id')
+}).catch(error => console.log(`Could not connect to database\n${error}`));
+}
+
+//deleteChar
+function deleteChar(req,res){
+let query = 'DELETE FROM hary WHERE id=1;'
+let values = [req.params.id]
+client.query(query,values).then(()=>{
+    res.redirect('/character/character_id')
+}).catch(error => console.log(`Could not connect to database\n${error}`));
+}
 
 
 
